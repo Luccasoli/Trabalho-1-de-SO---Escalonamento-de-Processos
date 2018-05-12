ef6319676ef6c3d81e4638e0480a2f52d451038c
luccasoli <oliveira.mesquita@aluno.uece.br>
2018-05-12 14:50:37 -0300

    Algoritmo sjf preemptivo adicionado

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 #define N_CLIENTES 100
-
+#define INTERVALO 10000
+#define QUANTUM 10 // Quantidade de somas
 // Declaração de funções
 
 int somatorio(int valor);
@@ -32,7 +33,7 @@ int gera_num(){
 
     // Choose a random mean between 1 and 10000
     default_random_engine e1(r());
-    uniform_int_distribution<int> uniform_dist(1, 10000);
+    uniform_int_distribution<int> uniform_dist(1, INTERVALO);
 
     int tempo = uniform_dist(e1);
 
@@ -157,15 +158,94 @@ void sjf_nao_preemptivo(){
 
 // SJF PREEMPTIVO
 struct Numero{
-    double tempo = 1000;
-    int onde_soma_parou = 0;
-    int num_gerado;
+    int posicao = 0;
+    int concluida = 0;
+    int qnt_somas_realizadas = 0;
+    int num_gerado = 0;
+    int num_atual = 1;
+    int soma_atual = 0;
 };
+
+void gera_num_sjf_preemptivo(queue< Numero > * q){
+    //int num_gerado;
+    int pos = 1;
+    for(int i = 1; i <= N_CLIENTES; i++){
+        m.lock();
+
+        Numero num;
+        num.num_gerado = gera_num();
+        num.posicao = pos++;
+        t_gera_num += num.num_gerado;
+        
+        cout << "\nGERADOR: Numero de posicao " << i << " foi gerado: " << num.num_gerado << " e demora " << t_gera_num/100 << "ms\n";
+        q->push(num);
+        
+        
+        m.unlock();
+        dorme_milisegundos(num.num_gerado/100);
+    }
+}
+
+void somatorio_sjf_preemptivo(Numero* num){
+
+    int i;
+    for(i = num->num_atual; i <= num->num_gerado; i++){
+        num->soma_atual += i;
+        num->qnt_somas_realizadas++;
+
+        cout << "Numero: " << num->num_gerado << ", de posicao: " << num->posicao << ", o somatorio agora vale: " << num->soma_atual << "\n";
+        
+        if(num->qnt_somas_realizadas % QUANTUM == 0 and num->qnt_somas_realizadas < num->num_gerado){
+            num->num_atual = i+1;
+            cout << "Quantum = " << QUANTUM << " estourou do numero: " << num->num_gerado << "\n";
+            break;
+        }
+        //dorme_milisegundos(1);
+    }
+    if(i >= num->num_gerado){
+        num->concluida = 1;
+        cout << "Numero: " << num->num_gerado << ", de posicao: " << num->posicao << ", concluido, resultado: " << num->soma_atual << '\n';
+    }
+    else
+        num->concluida = 0;
+    }
+
+void sjf_preemptivo(){
+    queue< Numero > qn;
+    int cont_num_processados = 1;
+    thread escalonador;
+    thread gerador_de_proc = thread(gera_num_sjf_preemptivo, &qn);
+
+    while(1){
+        if(!qn.empty()){
+            m.lock();
+
+            escalonador = thread(somatorio_sjf_preemptivo, &qn.front());
+            escalonador.join();
+            //cout << qn.front().num_gerado << " " << qn.front().qnt_somas_realizadas << '\n';
+            if(!qn.front().concluida){
+                qn.push(qn.front());
+                cout << "\nIndo para o proximo da fila!\n";
+            }
+            else{
+                cont_num_processados++;
+                cout << "\nConcluiu\n\n";
+            }
+            qn.pop();
+            m.unlock();
+        }
+        if(cont_num_processados > N_CLIENTES)
+            break;
+    }
+    gerador_de_proc.join();
+
+}
 
 int main(){
 
-    fcfs();
+    //fcfs();
     //sjf_nao_preemptivo();
+    sjf_preemptivo();
     
     return 0;
 }
